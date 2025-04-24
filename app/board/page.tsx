f954127284b92e084e97b6f3476f07786d824c17
DESKTOP-1GRL7B5\lhd57 <lhd5798@gmail.com>
2025-04-24 20:14:27 +0900

    신규

--- app/board/page.tsx
+++
@@ -1,9 +0,0 @@
-import Board from '../../components/Board';
-
-export default function BoardPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <Board />
-    </div>
-  );
-} 
