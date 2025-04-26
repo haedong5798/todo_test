a4202eb10e2b8cf1e045a57350744665f2d42758
DESKTOP-1GRL7B5\lhd57 <lhd5798@gmail.com>
2025-04-26 13:47:52 +0900

    no message

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
